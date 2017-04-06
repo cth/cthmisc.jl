2ffa49354ed818b7bb090558e86a1c0ff02eb38f
cth <christiantheilhave@gmail.com>
2017-04-06 09:54:28 +0200

    a couple of funcs

--- src/other.jl
+++ src/other.jl
@@ -9,3 +9,8 @@ mkdir_ifnot(dir) = if (!isdir(dir)) mkdir(dir) end
 "Decimal numbers from danish software often use , rather than . as separator"
 parse_danish_float(x) = parse(Float64, replace(x, ",", "."))
 
+"Count the number of lines in a file"
+count_lines(file) = length(split(readstring(file),'\n'))
+
+"Filters array to include only variables that have been defined (vectorized operation)"
+vals_if_defined(x) = map(x->@eval($x), filter(isdefined,x))
